177ac7844723974c65af9a7fc14e3e08e9c9a869
Santanu Bera <staunan.ebra@gmail.com>
2021-10-07 18:56:21 +0530

    Setup Font Awesome

---
+++ src/components/Header/MyHeader.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="header">
+		<div class="drawer_trigger" @click="handleDrawerClick">
+			<font-awesome-icon :icon="myIcon" />
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+export default {
+	data() {
+		return {
+			myIcon: faBars,
+		};
+	},
+	components: {
+		FontAwesomeIcon,
+	},
+	methods: {
+		handleDrawerClick(){
+			this.$emit("drawer");
+		}
+	}
+};
+</script>
+<style type="text/css" scoped>
+.header {
+	height: 50px;
+	width: 100%;
+	background: #cfd6ff;
+	display: flex;
+	align-items: center;
+}
+.drawer_trigger {
+	height: 50px;
+	width: 50px;
+	cursor: pointer;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.drawer_trigger:hover{
+	background: #333;
+	color: #fff;
+}
+</style>
